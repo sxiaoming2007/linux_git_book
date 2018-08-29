f14e09715408b8ffc8e6157ab1c08decaea88bb0
sunxiaoming@cmcm.com <sxiaoming2007@gmail.com>
2018-08-29 11:40:03 +0800

    modify

--- pre_build.py
+++ pre_build.py
@@ -1,12 +1,12 @@
 import os
 
-resources_path_dir = os.path.abspath('.')
+resources_path_dir = os.path.abspath('.') + os.sep + 'source'
 resources_index_file = resources_path_dir + os.sep + 'index.rst'
 values = []
 
 
 def config_dir(dir):
-    index_file = dir + os.sep + "index.rst"
+    index_file = os.path.join(resources_path_dir, dir + os.sep + "index.rst")
     if os.path.exists(index_file):
         with open(index_file, "r", encoding="utf-8") as file_read:
             read_lines = file_read.readlines()
@@ -20,13 +20,12 @@ def config_dir(dir):
                 if 'config_start' in content:
                     break
 
-        current_path = os.path.abspath(dir)
+        current_path = os.path.join(resources_path_dir, dir)
         current_file_list = os.listdir(current_path)
         filter_file = [file for file in current_file_list
-                       if os.path.isfile(dir + os.sep + file)
+                       if os.path.isfile(os.path.join(resources_path_dir, dir) + os.sep + file)
                        and file != 'index.rst'
                        and (file.endswith('.rst') or file.endswith('.md'))]
-
         for file in filter_file:
             name = '    ' + file.split('.')[0] + '\n'
             with open(index_file, 'a', encoding="utf-8") as file_write:
@@ -34,6 +33,7 @@ def config_dir(dir):
 
         with open(resources_index_file, "r", encoding="utf-8") as file_read:
             index_read_lines = file_read.readlines()
+            print(index_read_lines)
 
         with open(resources_index_file, 'w', encoding="utf-8") as file_write:
             for content in index_read_lines:
@@ -50,8 +50,8 @@ def config_dir(dir):
 
 if __name__ == '__main__':
     files_list = os.listdir(resources_path_dir)
-    filter_files = [file for file in files_list if os.path.isdir(file) and not file.startswith(".")]
-
+    filter_files = [file for file in files_list if os.path.isdir(os.path.join(resources_path_dir, file))
+                    and not file.startswith(".")]
     for file in filter_files:
         config_dir(file)
 
